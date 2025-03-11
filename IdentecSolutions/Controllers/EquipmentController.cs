bfd5f56eb77b93447777f360edac5339dc0efcaf
Tamara <Tamara@DESKTOP-Q0GEJ10>
2025-03-11 11:55:31 -0700

    Add project files.

---
+++ IdentecSolutions/Controllers/EquipmentController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentecSolutions.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+//[Route("api/[controller]")]
+//[ApiController]
+//TBD
+public class EquipmentController : ControllerBase
+{
+  
+}
